0bc424e9663fdaaf36572284a564f2aea4c0d795
frost02ben <167897790+frost02ben@users.noreply.github.com>
2024-05-07 11:46:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 AOS.init();
 
+$(document).ready(function() {
+    $('#cv').on('slide.bs.carousel', function (e) {
+      // Remove the active class from all header items
+      $('.cv-header-item').removeClass('active');
+      // Add the active class to the header item corresponding to the new active slide
+      $('.cv-header-item[data-bs-slide-to="' + e.to + '"]').addClass('active');
+    });
+  });
