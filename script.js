7ddceab8108e4efd76a2eb5c4e3e0e7233898bbe
frost02ben <167897790+frost02ben@users.noreply.github.com>
2024-05-08 12:46:30 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ $(document).ready(function() {
       $('.cv-header-item[data-bs-slide-to="' + e.to + '"]').addClass('active');
     });
   });
+
+  function downloadPDF() {
+    // Replace 'path_to_your_pdf.pdf' with the actual path to your PDF file
+    var pdfUrl = '/Assets/documents/ben-frost-cv.pdf';
+    
+    // Create a temporary anchor element
+    var anchor = document.createElement('a');
+    
+    // Set the href attribute to the PDF file URL
+    anchor.href = pdfUrl;
+    
+    // Set the download attribute to force download
+    anchor.download = 'ben-frost-cv.pdf';
+    
+    // Programmatically trigger a click event on the anchor element
+    anchor.click();
+  }
