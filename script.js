2f30908462c20de7030f1ac282dcf11a654da383
frost02ben <167897790+frost02ben@users.noreply.github.com>
2024-06-07 11:01:48 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,27 +2,19 @@ AOS.init();
 
 $(document).ready(function() {
     $('#cv').on('slide.bs.carousel', function (e) {
-      // Remove the active class from all header items
+
       $('.cv-header-item').removeClass('active');
-      // Add the active class to the header item corresponding to the new active slide
+
       $('.cv-header-item[data-bs-slide-to="' + e.to + '"]').addClass('active');
     });
   });
 
   function downloadPDF() {
-    // Replace 'path_to_your_pdf.pdf' with the actual path to your PDF file
-    var pdfUrl = '/Assets/documents/ben-frost-cv.pdf';
-    
-    // Create a temporary anchor element
+    var pdfUrl = '/Assets/documents/ben-frost-cv.pdf';   
     var anchor = document.createElement('a');
-    
-    // Set the href attribute to the PDF file URL
+  
     anchor.href = pdfUrl;
-    
-    // Set the download attribute to force download
     anchor.download = 'ben-frost-cv.pdf';
-    
-    // Programmatically trigger a click event on the anchor element
     anchor.click();
   }
 
@@ -36,3 +28,22 @@ $(document).ready(function() {
     document.documentElement.style.setProperty('--blue', newColor);
   });
 
+  window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('.navbar');
+    if (window.scrollY > 700) { 
+      navbar.classList.add('scrolled-bg');
+    } else {
+      navbar.classList.remove('scrolled-bg');
+    }
+  });
+
+  window.addEventListener('scroll', function() {
+    var navLinks = document.querySelectorAll('.nav-link');
+    navLinks.forEach(function(navLink) {
+      if (window.scrollY > 700) { 
+        navLink.classList.add('scrolled-font');
+      } else {
+        navLink.classList.remove('scrolled-font');
+      }
+    });
+  });
