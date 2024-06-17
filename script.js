afccd03b055daa3fbde0e1d1e2acc9ade18489b8
frost02ben <167897790+frost02ben@users.noreply.github.com>
2024-06-17 11:18:55 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -9,20 +9,13 @@ AOS.init();
     anchor.click();
   }
 
-  document.getElementById('colorPickerOne').addEventListener('input', function(event) {
-    const newColor = event.target.value;
-    document.documentElement.style.setProperty('--green', newColor);
-  });
-
-  document.getElementById('colorPickerTwo').addEventListener('input', function(event) {
-    const newColor = event.target.value;
-    document.documentElement.style.setProperty('--blue', newColor);
-  });
+  
 
   window.addEventListener('scroll', function() {
-    var navbar = document.querySelector('.navbar');
-    if (window.scrollY > 700) { 
+    var navbar = document.querySelector('.nav-primary');
+    if (window.scrollY > 600) { 
       navbar.classList.add('scrolled-bg');
+      navbar.classList.remove('nav-alt');
     } else {
       navbar.classList.remove('scrolled-bg');
     }
@@ -31,11 +24,58 @@ AOS.init();
   window.addEventListener('scroll', function() {
     var navLinks = document.querySelectorAll('.nav-link');
     navLinks.forEach(function(navLink) {
-      if (window.scrollY > 700) { 
+      if (window.scrollY > 600) { 
         navLink.classList.add('scrolled-font');
       } else {
         navLink.classList.remove('scrolled-font');
       }
     });
   });
+
+function setbgColor(bgAlt) {
+  document.documentElement.style.setProperty('--bg-color', bgAlt);
+}
+
+function setfontColor(fontAlt) {
+  document.documentElement.style.setProperty('--font-color', fontAlt);
+}
+
+function applyDarkMode(isDarkMode) {
+  const darkBtn = document.getElementById('dark-btn');
+  const icon = darkBtn.querySelector('i');
+  const text = darkBtn.querySelector('span'); 
+
+  if (isDarkMode) {
+    setbgColor('#2b2b2b'); // Dark background color
+    setfontColor('#fff'); // Light font color
+    icon.classList.remove('fa-moon');
+    icon.classList.add('fa-sun');
+    text.textContent = 'Light Mode';
+  } else {
+    setbgColor('#fff'); // Default background color
+    setfontColor('#000'); // Default font color
+    icon.classList.remove('fa-sun');
+    icon.classList.add('fa-moon');
+    text.textContent = 'Dark Mode';
+  }
+}
+
+function toggleDarkMode() {
+  const isDarkMode = localStorage.getItem('isDarkMode') === 'true';
+  localStorage.setItem('isDarkMode', !isDarkMode);
+  applyDarkMode(!isDarkMode);
+}
+
+// Event listener for the button
+document.getElementById('dark-btn').addEventListener('click', toggleDarkMode);
+
+// Apply the stored mode on page load
+window.addEventListener('DOMContentLoaded', (event) => {
+  const isDarkMode = localStorage.getItem('isDarkMode') === 'true';
+  applyDarkMode(isDarkMode);
+});
+
+
+
+
 
